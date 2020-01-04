8f127ae5655650efab89cbc1e3666f0161b1a5b6
Stacy <enjoyorhate@gmail.com>
2020-01-04 19:37:25 +0200

    - added fonts and root styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
 import './stylesheet/main.css';
+import './stylesheet/fonts.css';
+import './stylesheet/vars.css';
 
 ReactDOM.render(
   <Provider store={store}>
