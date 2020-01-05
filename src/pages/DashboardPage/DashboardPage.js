fce2d5d16b4c68c5df2a0cf72e6dba5ed759375b
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2020-01-06 01:38:04 +0200

    Currency styles & Html for all devices

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Currency from '../../components/Currency/Currency';
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <>
+      <div>DashboardPage</div>
+      <Currency />
+    </>
+  );
 };
 
 export default DashboardPage;
