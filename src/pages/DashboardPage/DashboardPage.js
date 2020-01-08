15ec3746e08ca41b0de740f592736e2fe5c96640
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2020-01-08 12:54:17 +0200

    fix dashboadr

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import styles from './DashboardPage.module.css';
 // import HomeTab from '../../components/HomeTab/HomeTab';
-// import DiagramTab from '../../components/DiagramTab/DiagramTab';
+import DiagramTab from '../../components/DiagramTab/index';
 // import Header from '../../components/Header/Header';
 // import Currency from '../../components/Currency/Currency'
 // import ModalAddTransaction from '../../components/ModalAddTransaction/ModalAddTransaction'
@@ -12,30 +13,33 @@ export default class DashboardPage extends Component {
   }
 
   render() {
+    const windowWidth = document.documentElement.clientWidth;
     return (
       <div className={styles.container}>
         <header className={styles.header}>{/*
-    <Header />> */}</header>
+    <Header />> */}Header</header>
         <main className={styles.main}>
           <aside className={styles.aside}>
-            <nav className={styles.nav} />
-            <section className={styles.balance} />
-            {window.screen.width > 769 && (
+            <nav className={styles.nav}>Navigation </nav>
+            <section className={styles.balance}>Balance</section>
+            {windowWidth >= 768 && (
               <section className={styles.currency}>
-                {/*
-        <Currency /> */}
+                Curr
+                {/* <Currency /> */}
               </section>
             )}
           </aside>
           <article className={styles.content}>
-            {/*
+            <Switch>
+              {/*
       <Route path="/home" component={HomeTab} /> */}
-            {/*
-      <Route path="/diagram" component={DiagramTab} /> */}
+
+              <Route path="/diagram" component={DiagramTab} />
+            </Switch>
           </article>
         </main>
         <div className={styles.addTransaction}>
-          {/*
+          +{/*
     <ModalAddTransaction /> */}
         </div>
       </div>
