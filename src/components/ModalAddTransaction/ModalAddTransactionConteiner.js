09bae11d62b3da9e6535cbe9782e3fb576d3413f
alexscub <alexscub1@gmail.com>
2020-01-12 19:50:33 +0200

    addFaviconAndSomeAddtransactionFixes

--- src/components/ModalAddTransaction/ModalAddTransactionConteiner.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { closeModalAddTransaction } from '../../redux/global/globalActions';
-import ModalAddTransaction from './ModalAddTransaction';
-
-const mapDispatchToProps = {
-  closeModalAddTransaction,
-};
-
-export default connect(null, mapDispatchToProps)(ModalAddTransaction);
