4e8072e1de5c7365b16134d85ea48f4326f4b798
dmb-2222 <dmb-2222@yandex.ru>
2020-01-14 17:09:12 +0200

    add financeAction, Reducer, connect to rootReducer

--- src/redux/global/globalReducers.js
+++ src/redux/global/globalReducers.js
@@ -11,7 +11,18 @@ const isModalAddTransactionOpen = (state = false, { type }) => {
       return state;
   }
 };
+const isModalLogoutOpen = (state = false, { type }) => {
+  switch (type) {
+    case types.OPEN_MODALLGOUTTRANSACTION:
+      return true;
+    case types.CLOSE_MODALGOUTTRANSACTION:
+      return false;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
   isModalAddTransactionOpen,
+  isModalLogoutOpen,
 });
