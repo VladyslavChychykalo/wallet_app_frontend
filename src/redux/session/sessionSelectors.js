e33827ce474d66d7a6b0f83b89d2abe1d3147485
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-01-18 12:28:48 +0200

    added registration form validation

--- src/redux/session/sessionSelectors.js
+++ src/redux/session/sessionSelectors.js
@@ -3,3 +3,5 @@ export const getIsAuth = store => store.session.isAuth;
 export const getUser = store => store.session.user;
 
 export const getToken = store => store.session.token;
+
+export const getError = store => store.session.error;
