920ac94399c58d732e3abb5965ed117da3bebaa0
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2020-01-19 17:16:43 +0200

    tooltip fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,17 +17,17 @@ const Header = () => {
     <div className={styles.header}>
       <div className={styles.Logo_box}>
         <img src={LogoIMG} className={styles.Logo_IMG} alt="Wallet" data-tip />
-        {windowWidth >= 768 && (
-          <ReactTooltip
-            className={styles.tooltip}
-            effect="solid"
-            place="bottom"
-            event="click focus"
-            offset={{ top: 0, right: 50 }}
-            globalEventOff="mouseout"
-            getContent={() => tips[Math.floor(Math.random() * tips.length)]}
-          />
-        )}
+        <ReactTooltip
+          className={styles.tooltip}
+          effect="solid"
+          place="bottom"
+          event="click focus"
+          offset={
+            windowWidth >= 768 ? { top: 0, right: 50 } : { top: 0, right: 140 }
+          }
+          globalEventOff="mouseout"
+          getContent={() => tips[Math.floor(Math.random() * tips.length)]}
+        />
         <Link to="/" alt="homepage" className={styles.Logo_link}>
           <h1 className={styles.title}>Wallet</h1>
         </Link>
