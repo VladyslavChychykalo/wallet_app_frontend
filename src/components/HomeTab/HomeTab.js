bf5cef166aa3e346f0e7ac2606e2f7f467ad3a64
dmb-2222 <dmb-2222@yandex.ru>
2020-01-19 17:51:05 +0200

    checkerr

--- src/components/HomeTab/HomeTab.js
+++ src/components/HomeTab/HomeTab.js
@@ -41,7 +41,7 @@ class HomeTab extends React.Component {
             <div>Balance</div>
             <div className={styles.textCenter}>Delete</div>
           </div>
-          {typeof transactions === 'object' && transactions.length === 0 ? (
+          {!Array.isArray(transactions) || transactions.length === 0 ? (
             // {transactions.length === 0 ? (
             <div className={styles.addTransaction}>Please add transaction</div>
           ) : (
